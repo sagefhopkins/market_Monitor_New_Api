ae1441f1a129dbb8254e6a37cab3baf1ece01106
sagefhopkins <sagefhopkins@gmail.com>
2018-10-13 18:35:50 +0900

    Updates

--- new_Finder.py
+++ new_Finder.py
@@ -11,7 +11,15 @@ def search_Url(name):
     url_Search = requests.get(search)
     print search
     soup = BeautifulSoup(url_Search.text)
-    all_Links = soup.findAll('a')
+    num = 1
+    arr = []
 
-    for link in all_Links:
-        if link.get('href')
+#This formats the urls so they are complete, however after each is generated we need to ensure only the second value is the list create is taken because that is the real url, the other one
+#is a google cache and is not helpful
+    for link in  soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
+        result = re.split(":(?=http)",link["href"].replace("/url?q=",""))
+        #final = re.search(r'\'(.*)\', \'(.*?)\'', str(result), '')
+        #print final.result(1)
+        arr.insert(num ,result)
+        num = num + 1
+    print 'Array Number 10' + str(arr[10])
