a5de4086359af0b5c450acee611fc3c5f089a09e
Abdul Chandar <43941085+abdulchandar@users.noreply.github.com>
2018-10-27 22:57:47 -0400

    Added search for website

--- Web/market.py
+++ Web/market.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python2
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 import mysql.connector
+from base64 import b64encode as enc
+from base64 import b64decode as dec
 
 app = Flask(__name__)
 
@@ -17,11 +19,21 @@ db = mysql.connector.connect(host='107.181.191.59',
 def index():
     return render_template('index.html')
 
-@app.route('/commodities')
-def commodities():
+@app.route('/commodities', methods=['GET', 'POST'])
+def commodities_all():
+
+    if request.method == 'POST':
+        form = request.form
+        return redirect(url_for('commodities', query = enc(form['query']))) 
+    else:
+        return redirect(url_for('commodities', query = enc('.*')))
+
+@app.route('/commodities/<query>/')
+def commodities(query):
+    query = dec(query) 
     cursor = db.cursor()
-    sql = "SELECT * FROM Commodity_Data"
-    cursor.execute(sql)
+    sql = "SELECT * FROM Commodity_Data WHERE commodity REGEXP %s"
+    cursor.execute(sql, (query,))
     results = cursor.fetchall()
     return render_template('commodities.html', results=results)
 
