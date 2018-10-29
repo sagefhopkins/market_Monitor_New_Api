fdc2839a5c7a44b838c3f67acc721debae786fde
sagefhopkins <sagefhopkins@gmail.com>
2018-10-30 06:44:59 +0900

    Webserver features added

--- Web/market.py
+++ Web/market.py
@@ -7,13 +7,13 @@ from base64 import b64decode as dec
 
 app = Flask(__name__)
 
-db = mysql.connector.connect(host='107.181.191.59', 
-                            user='root', 
-                            passwd='P@$$06281a', 
+db = mysql.connector.connect(host='107.181.191.59',
+                            user='root',
+                            passwd='P@$$06281a',
                             database='cur_Monitor'
-                            ) 
-                           # user='reader', 
-                           # passwd='R#]7>y#q<AM', 
+                            )
+                           # user='reader',
+                           # passwd='R#]7>y#q<AM',
 
 @app.route('/')
 def index():
@@ -24,13 +24,13 @@ def commodities_all():
 
     if request.method == 'POST':
         form = request.form
-        return redirect(url_for('commodities', query = enc(form['query']))) 
+        return redirect(url_for('commodities', query = enc(form['query'])))
     else:
         return redirect(url_for('commodities', query = enc('.*')))
 
 @app.route('/commodities/<query>/')
 def commodities(query):
-    query = dec(query) 
+    query = dec(query)
     cursor = db.cursor()
     sql = "SELECT * FROM Commodity_Data WHERE commodity REGEXP %s"
     cursor.execute(sql, (query,))
@@ -39,18 +39,28 @@ def commodities(query):
 
 @app.route('/currency')
 def currency():
-    return render_template('currency.html')
+    query = dec(query)
+    cursor = db.cursor()
+    sql = "SELECT * FROM Currency_Data WHERE ticker REGEXP %s"
+    cursor.execute(sql, (query, ))
+    results = cursor.fetchall()
+    return render_template('currency.html', results=results)
 
 @app.route('/news')
 def news():
-    return render_template('news.html')
+    query = dec(query)
+    cursor = db.cursor()
+    sql = "SELECT * FROM News_Data WHERE stock REGEXP %s"
+    cursor.execute(sql, (query, ))
+    results = cursor.fetchall()
+    return render_template('news.html', results=results)
 
 
 @app.after_request
 def add_header(response):
-    response.cache_control.no_cache = True 
-    response.cache_control.no_store = True 
-    response.cache_control.must_revalidate = True 
+    response.cache_control.no_cache = True
+    response.cache_control.no_store = True
+    response.cache_control.must_revalidate = True
     return response
 
 if __name__ == '__main__':
